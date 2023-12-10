c661174ee4445a6d19860fcb093d8cddaf3fae61
Obinna Anderson Asiegbulam <oasiegbulam@gmail.com>
2023-12-10 11:18:47 +0100

    hide google secret

--- server/src/MarthasLibrary.IdentityServer/HostingExtensions.cs
+++ server/src/MarthasLibrary.IdentityServer/HostingExtensions.cs
@@ -12,95 +12,95 @@ using System.Reflection;
 
 namespace MarthasLibrary.IdentityServer
 {
-  internal static class HostingExtensions
-  {
-    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    internal static class HostingExtensions
     {
-      builder.Services.AddRazorPages();
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
 
-      builder.Services.AddDbContext<ApplicationDbContext>(options =>
-          options.UseSqlServer(builder.Configuration.GetConnectionString("Identity")));
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Identity")));
 
-      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-          .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>()
-          .AddEntityFrameworkStores<ApplicationDbContext>()
-          .AddDefaultTokenProviders();
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
-      // builder.Services.AddScoped<ApplicationUserClaimsPrincipalFactory>();
-      builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
+            // builder.Services.AddScoped<ApplicationUserClaimsPrincipalFactory>();
+            builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
 
-      builder.Services.AddScoped<IProfileService, CustomProfileService>();
+            builder.Services.AddScoped<IProfileService, CustomProfileService>();
 
-      var migrationsAssembly = typeof(Program).GetTypeInfo()
-          .Assembly.GetName().Name;
+            var migrationsAssembly = typeof(Program).GetTypeInfo()
+                .Assembly.GetName().Name;
 
-      builder.Services
-          .AddIdentityServer(options =>
-          {
-            options.Events.RaiseErrorEvents = true;
-            options.Events.RaiseInformationEvents = true;
-            options.Events.RaiseFailureEvents = true;
-            options.Events.RaiseSuccessEvents = true;
-            options.EmitStaticAudienceClaim = true;
-          })
-          .AddConfigurationStore(options =>
-          {
-            options.ConfigureDbContext = optionsBuilder =>
-                      optionsBuilder.UseSqlServer(
-                          builder.Configuration
-                              .GetConnectionString("IdentityServer"),
-                          sqlOptions => sqlOptions
-                              .MigrationsAssembly(migrationsAssembly));
-          })
-          .AddConfigurationStoreCache()
-          .AddOperationalStore(options =>
-          {
-            options.ConfigureDbContext = optionsBuilder =>
-                      optionsBuilder.UseSqlServer(builder.Configuration
-                              .GetConnectionString("IdentityServer"),
-                          sqlOptions => sqlOptions
-                              .MigrationsAssembly(migrationsAssembly));
-            options.EnableTokenCleanup = true;
-          })
-          .AddAspNetIdentity<ApplicationUser>()
-          .AddProfileService<CustomProfileService>();
+            builder.Services
+                .AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                    options.EmitStaticAudienceClaim = true;
+                })
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = optionsBuilder =>
+                        optionsBuilder.UseSqlServer(
+                            builder.Configuration
+                                .GetConnectionString("IdentityServer"),
+                            sqlOptions => sqlOptions
+                                .MigrationsAssembly(migrationsAssembly));
+                })
+                .AddConfigurationStoreCache()
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = optionsBuilder =>
+                        optionsBuilder.UseSqlServer(builder.Configuration
+                                .GetConnectionString("IdentityServer"),
+                            sqlOptions => sqlOptions
+                                .MigrationsAssembly(migrationsAssembly));
+                    options.EnableTokenCleanup = true;
+                })
+                .AddAspNetIdentity<ApplicationUser>()
+                .AddProfileService<CustomProfileService>();
 
-      builder.Services.AddAuthentication()
-          .AddGoogle(options =>
-          {
-            options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-            options.ClientId = "408144084342-edugfoip6oslup3lh7hln5fohr1jno7f.apps.googleusercontent.com";
-            options.ClientSecret = "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";
-          });
+            builder.Services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.ClientId = "";
+                    options.ClientSecret = "";
+                });
 
-      builder.Services.Configure<ForwardedHeadersOptions>(options =>
-      {
-        options.ForwardedHeaders = ForwardedHeaders.XForwardedFor
-                                         | ForwardedHeaders.XForwardedProto;
-      });
+            builder.Services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                                           | ForwardedHeaders.XForwardedProto;
+            });
 
-      return builder.Build();
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseForwardedHeaders();
+            app.UseSerilogRequestLogging();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.MapRazorPages()
+                .RequireAuthorization();
+
+            return app;
+        }
     }
-
-    public static WebApplication ConfigurePipeline(this WebApplication app)
-    {
-      app.UseForwardedHeaders();
-      app.UseSerilogRequestLogging();
-
-      if (app.Environment.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseStaticFiles();
-      app.UseRouting();
-      app.UseIdentityServer();
-      app.UseAuthorization();
-
-      app.MapRazorPages()
-          .RequireAuthorization();
-
-      return app;
-    }
-  }
 }
